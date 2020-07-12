750e48188839c7340b81121805bcaf2880996562
xxnmxx <34981605+xxnmxx@users.noreply.github.com>
2020-07-12 23:26:16 +0900

    cmd

--- nn/cmd/exp/main.go
+++ nn/cmd/exp/main.go
@@ -7,23 +7,44 @@ import (
 )
 
 func main() {
-	//input := []float64{1, 0.5}
+	input := []float64{1, 0.5}
 
 	w1 := [][]float64{
 		[]float64{0.1, 0.3, 0.5},
-		[]float64{0.2, 0.4, 0, 6},
+		[]float64{0.2, 0.4, 0.6},
 	}
 	b1 := []float64{0.1, 0.2, 0.3}
-	a := nn.Sigmoid
 	n1 := nn.NewNetwork()
-	n1.SetWeight(w1)
-	n1.SetBias(b1)
-	n1.SetActivator(nn.Sigmoid)
+	n1.SetAll(w1, b1, nn.Sigmoid)
+	out1 := n1.Forword(input)
+
+	w2 := [][]float64{
+		[]float64{0.1, 0.4},
+		[]float64{0.2, 0.5},
+		[]float64{0.3, 0.6},
+	}
+	b2 := []float64{0.1, 0.2}
+
 	n2 := nn.NewNetwork()
-	n2.SetAll(w1,b1,a)
-	//wed1 := nn.InnerProduct(input, w1)
-	//bed1 := nn.Add(wed1, b1)
-	//acted := nn.Activate(bed1, nn.Sigmoid)
-	fmt.Println(n1)
-	fmt.Println(n2)
+	n2.SetAll(w2, b2, nn.Sigmoid)
+	out2 := n2.Forword(out1)
+
+	w3 := [][]float64{
+		[]float64{0.1,0.3},
+		[]float64{0.2,0.4},
+	}
+	b3 := []float64{0.1,0.2}
+	n3 := nn.NewNetwork()
+	n3.SetAll(w3,b3,nn.Identity)
+	out3 := n3.Forword(out2)
+	final := nn.SoftMax(out3)
+	fmt.Println(out1)
+	fmt.Println(out2)
+	fmt.Println(out3)
+	fmt.Println(final)
+
+	t := []float64{0.3,2.9,4.0}
+	testMax := nn.SoftMax(t)
+	fmt.Println(testMax)
+
 }
--- nn/nn.go
+++ nn/nn.go
@@ -1,15 +1,16 @@
 package nn
 
 import (
+	"fmt"
 	"log"
 	"math"
 )
 
 // Network
 type Network struct {
-	W [][]float64
-	B []float64
-	A activator
+	Weights   [][]float64
+	Baiases   []float64
+	Activator activator
 }
 
 func NewNetwork() *Network {
@@ -19,35 +20,47 @@ func NewNetwork() *Network {
 // Method
 // Setter
 func (n *Network) SetAll(ws [][]float64, bs []float64, a activator) {
-	n.W, n.B, n.A = ws, bs, a
+	n.Weights, n.Baiases, n.Activator = ws, bs, a
 }
 
 func (n *Network) SetWeight(ws [][]float64) {
-	n.W = ws
+	n.Weights = ws
 }
 
 func (n *Network) SetBias(bs []float64) {
-	n.B = bs
+	n.Baiases = bs
 }
 
 func (n *Network) SetActivator(a activator) {
-	n.A = a
+	n.Activator = a
 }
 
 // Checker
-func (n *Network) checkShape(ins []float64) bool {
-	if len(ins) != n.W {
-		fmt.Printf("shape unmatched: ins: %v, ws: [%v,%v]",len(ins),len(n.W),len(n.W[0]))
-	}
-}
+// FixMe need to implement shape chacker etc...
 
 // Movement
-func (n *Network) Forword(ins []float64) *Netword {
-
+func (n *Network) Forword(ins []float64) []float64 {
+	if n.Weights == nil {
+		fmt.Println("Weights are nil.")
+		return nil
+	}
+	if n.Baiases == nil {
+		fmt.Println("Baiases are nil.")
+		return nil
+	}
+	if n.Activator == nil {
+		fmt.Println("Activator is nil.")
+		return nil
+	}
+	iped := InnerProduct(ins,n.Weights)
+	added := Add(iped,n.Baiases)
+	outs := Activate(added,n.Activator)
+	return outs
 }
 
 // Activators
 type activator func(float64) float64
+
 func Sigmoid(in float64) float64 {
 	return 1 / (1 + math.Pow(math.E, -in))
 }
@@ -59,33 +72,55 @@ func ReLU(in float64) float64 {
 	return 0
 }
 
+func Identity(in float64) float64 {
+	return in
+}
+
+// Outputter
+type outputter func([]float64) []float64
+
+func SoftMax(ins []float64) []float64 {
+	max := 0.0
+	for _, in := range ins {
+		max = math.Max(max,in)
+	}
+	sum := 0.0
+	for _, in := range ins {
+		sum += math.Exp(in-max)
+	}
+	outs := make([]float64,len(ins))
+	for i, in := range ins {
+		outs[i] = math.Exp(in-max) / sum
+	}
+	return outs
+}
+
 // Array Operation
 func InnerProduct(ins []float64, ws [][]float64) []float64 {
 	// Check the shape
 	if len(ins) != len(ws) {
-		log.Fatalf("shape unmatched:ins %v, ws %v,%v\n",len(ins),len(ws),len(ws[0]))
+		log.Fatalf("shape unmatched:ins %v, ws [%v:%v]\n", len(ins), len(ws), len(ws[0]))
 	}
 	outs := make([]float64, len(ws[0]))
 	for i, in := range ins {
 		for j, w := range ws[i] {
-			outs[j] += in*w
+			outs[j] += in * w
 		}
 	}
 	return outs
 }
 
-//func Add(ins []float64, add []float64) []float64 {
-//	for i, in := range ins {
-//		ins[i] += add
-//	}
-//	return outs
-//}
+func Add(ins []float64, add []float64) []float64 {
+	for i, _ := range ins {
+		ins[i] += add[i]
+	}
+	return ins
+}
 
 func Activate(ins []float64, f activator) []float64 {
-	out := make([]float64,len(ins))
+	out := make([]float64, len(ins))
 	for i, in := range ins {
 		out[i] = f(in)
 	}
 	return out
 }
-
